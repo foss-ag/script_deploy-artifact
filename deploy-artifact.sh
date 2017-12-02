fc03eadff59589900e2ae7e0a293e8d208c1f0f9
Michael Gajda <michael.gajda@tu-dortmund.de>
2017-12-02 21:31:47 +0100

    Clean up after deployment

--- deploy-artifact.sh
+++ deploy-artifact.sh
@@ -11,7 +11,8 @@ USAGE="$0 <FILE/DIR>"
 DIR=$(basename ${TRAVIS_SLUG})-${TRAVIS_BRANCH}
 [ -z $DIR ] && echo "Travis env not found" && exit 1
 wget https://raw.githubusercontent.com/foss-ag/script_curl-dirup/v1.0.0/curl-dirup.sh -O curl-dirup.sh && chmod +x curl-dirup.sh
+rm -rf $DIR 2>/dev/null
 mkdir -p ${DIR}
 cp -a $@ ${DIR}
 ./curl-dirup.sh https://cloud.foss-ag.de/remote.php/webdav/foss-ag_buildbot foss-ag_buildbot:$PDFPW ${DIR}
-
+rm -rf $DIR 2>/dev/null
