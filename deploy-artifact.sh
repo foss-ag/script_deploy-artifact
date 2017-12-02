55aaefd931bd2847f6b1e23efd29b589439a015b
Michael Gajda <michael.gajda@tu-dortmund.de>
2017-12-02 22:26:23 +0100

    Use correct ENV varible

--- deploy-artifact.sh
+++ deploy-artifact.sh
@@ -8,7 +8,7 @@ USAGE="$0 <FILE/DIR>"
 # check parameter
 : ${1?Usage: $USAGE}
 
-DIR=$(basename ${TRAVIS_SLUG})-${TRAVIS_BRANCH}
+DIR=$(basename ${TRAVIS_REPO_SLUG})-${TRAVIS_BRANCH}
 [ -z $DIR ] && echo "Travis env not found" && exit 1
 wget https://raw.githubusercontent.com/foss-ag/script_curl-dirup/v1.0.1/curl-dirup.sh -O curl-dirup.sh && chmod +x curl-dirup.sh
 rm -rf $DIR 2>/dev/null
