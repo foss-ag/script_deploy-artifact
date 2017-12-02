2b670c060744de696c834022fe6a2427abe537f1
Michael Gajda <michael.gajda@tu-dortmund.de>
2017-12-02 20:52:07 +0100

    Add deploy script

---
+++ deploy-artifact.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# 2017 Michael Gajda <draget@speciesm.net>
+# See LICENSE for license information
+
+USAGE="$0 <FILE/DIR>"
+
+# check parameter
+: ${1?Usage: $USAGE}
+
+DIR=$(basename ${TRAVIS_SLUG})-${TRAVIS_BRANCH}
+#wget https://raw.githubusercontent.com/foss-ag/script_curl-dirup/v1.0.0/curl-dirup.sh -O curl-dirup.sh && chmod +x curl-dirup.sh
+mkdir -p ${DIR}
+cp -a $1 ${DIR}
+./curl-dirup.sh https://cloud.foss-ag.de/remote.php/webdav/foss-ag_buildbot foss-ag_buildbot:$PDFPW ${DIR}
+
