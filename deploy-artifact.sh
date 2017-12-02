5213c0a6667395349a3cc8fbb8696d0815d9af73
Michael Gajda <michael.gajda@tu-dortmund.de>
2017-12-02 21:20:52 +0100

    Allow wildcards

--- deploy-artifact.sh
+++ deploy-artifact.sh
@@ -9,8 +9,9 @@ USAGE="$0 <FILE/DIR>"
 : ${1?Usage: $USAGE}
 
 DIR=$(basename ${TRAVIS_SLUG})-${TRAVIS_BRANCH}
-#wget https://raw.githubusercontent.com/foss-ag/script_curl-dirup/v1.0.0/curl-dirup.sh -O curl-dirup.sh && chmod +x curl-dirup.sh
+[ -z $DIR ] && echo "Travis env not found" && exit 1
+wget https://raw.githubusercontent.com/foss-ag/script_curl-dirup/v1.0.0/curl-dirup.sh -O curl-dirup.sh && chmod +x curl-dirup.sh
 mkdir -p ${DIR}
-cp -a $1 ${DIR}
+cp -a $@ ${DIR}
 ./curl-dirup.sh https://cloud.foss-ag.de/remote.php/webdav/foss-ag_buildbot foss-ag_buildbot:$PDFPW ${DIR}
 
