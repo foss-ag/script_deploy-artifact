5df1f8719e0d69bb44f7f516f9eb3f6c3b1b73b7
Michael Gajda <michael.gajda@tu-dortmund.de>
2018-05-10 21:18:28 +0200

    New URL and comments
    Add comments and the new URL for the shared directory

--- deploy-artifact.sh
+++ deploy-artifact.sh
@@ -8,11 +8,14 @@ USAGE="$0 <FILE/DIR>"
 # check parameter
 : ${1?Usage: $USAGE}
 
+# Target-dir will be repo and branch
 DIR=$(basename ${TRAVIS_REPO_SLUG})-${TRAVIS_BRANCH}
-[ -z $DIR ] && echo "Travis env not found" && exit 1
+[ -z $DIR ] && echo "$0: Could not find a Travis env" && exit 1
+# Creating a directory with an recursive upload is not possible with curl, use helper-script
 wget https://raw.githubusercontent.com/foss-ag/script_curl-dirup/v1.0.1/curl-dirup.sh -O curl-dirup.sh && chmod +x curl-dirup.sh
 rm -rf $DIR 2>/dev/null
 mkdir -p ${DIR}
 cp -a $@ ${DIR}
-./curl-dirup.sh https://cloud.foss-ag.de/remote.php/webdav/foss-ag_buildbot foss-ag_buildbot:$PDFPW ${DIR}
+# Actual upload
+./curl-dirup.sh https://cloud.foss-ag.de/public.php/webdav 7bm6b2A8zTdHBE4:$PDFPW ${DIR}
 rm -rf $DIR 2>/dev/null
