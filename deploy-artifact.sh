40d474ed3c6bcae51b13656d66359d7d69220927
Michael Gajda <michael.gajda@tu-dortmund.de>
2018-05-16 10:56:52 +0200

    Remove rm -rf
    Calling rm -rf on some variable could lead to dangerous things if this script is run on a productive machine and not an travis environment. Technically there is no need to clean up the output directory so it should be save to just remove this command.

--- deploy-artifact.sh
+++ deploy-artifact.sh
@@ -13,9 +13,8 @@ DIR=$(basename ${TRAVIS_REPO_SLUG})-${TRAVIS_BRANCH}
 [ -z $DIR ] && echo "$0: Could not find a Travis env" && exit 1
 # Creating a directory with an recursive upload is not possible with curl, use helper-script
 wget https://raw.githubusercontent.com/foss-ag/script_curl-dirup/v1.0.1/curl-dirup.sh -O curl-dirup.sh && chmod +x curl-dirup.sh
-rm -rf $DIR 2>/dev/null
-mkdir -p ${DIR}
+
+mkdir -p ${DIR} 2>/dev/null
 cp -a $@ ${DIR}
 # Actual upload
 ./curl-dirup.sh https://cloud.foss-ag.de/public.php/webdav 7bm6b2A8zTdHBE4:$PDFPW ${DIR}
-rm -rf $DIR 2>/dev/null
